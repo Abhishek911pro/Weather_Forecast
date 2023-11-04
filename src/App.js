71a5e336ff165af7b5d650ed4e94be09bcccfb4d
Abhishek_kumar <Abhishku611@gmail.com>
2023-11-05 01:16:24 +0530

    complete design of weather data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React,{ useState } from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faTemperatureHigh } from '@fortawesome/free-solid-svg-icons'
+import { faTemperatureLow } from '@fortawesome/free-solid-svg-icons'
+import { faDroplet } from '@fortawesome/free-solid-svg-icons'
+import { faWind } from '@fortawesome/free-solid-svg-icons'
+import { faThermometer } from '@fortawesome/free-solid-svg-icons'
 import clear from './components/Assets/clear.png'
 import cloud from './components/Assets/cloud.png'
 import drizzle from './components/Assets/drizzle.png'
 import rain from './components/Assets/rain.png'
 import snow from './components/Assets/snow.png'
-import wind from './components/Assets/wind.png'
-import humidity from './components/Assets/humidity.png'
+
 
 
 import { useEffect } from 'react';
@@ -96,7 +100,7 @@ function App() {
         <div className='Appheader1 shadow'>
           <h1 className='Apptitle ms-3'>Weather Forecast</h1>
           <div className='searchbar me-3'>
-            <input type="Search" className='rounded-pill border ps-3' placeholder='...search' onChange={ (event)=>{
+            <input type="Search" className='rounded-pill border ps-3' placeholder='...City_name' onChange={ (event)=>{
                setCity(event.target.value);
             }}/>
             <button className="rounded-circle ms-1 shadow" onClick={ () =>{
@@ -108,8 +112,8 @@ function App() {
         <div className='Appheader2 '>
           <h2 className='ps-3 Apptitle2'>{city.name}</h2>
             <div className='dateTime'>
-              <h5 className='pe-5 Apptitle2'>{newTime}</h5>
-               <h5 className='pe-3 Apptitle2'>{newdate}</h5>
+              <h5 className='pe-5 Apptitle2'>{newTime == "Invalid Date" ? <h4></h4>: newTime}</h5>
+               <h5 className='pe-3 Apptitle2'>{newdate == "NaN-Invalid Date-NaN" ? <h4></h4>: newdate}</h5>
             </div>
         </div>
         <hr />
@@ -118,9 +122,9 @@ function App() {
             <div className='weather'>
               <img src={icon} alt="" className='mainIcon'/>
               <div className='ms-4'>
-                {city.main ? <h1>{Math.trunc(city.main.temp)}&deg;Cel</h1> : <h1></h1>}
+                {city.main ? <h1 className='fw-normal'>{Math.trunc(city.main.temp)}&deg;Cel</h1> : <h1></h1>}
                 {city.weather && city.weather.length > 0 ? (
-                <h1>{city.weather[0].main}</h1>
+                <h1 className='fw-normal'>{city.weather[0].main}</h1>
                 ) : (
                   <h1></h1>
               )}
@@ -133,74 +137,45 @@ function App() {
           </div>
           <div className='col-sm-3 '></div>
           <div className='col-sm-5 p-4'>
-              <div className='weatherReport mb-3'>
-                <div>
-                  {city.main ? <h6>{Math.trunc(city.main.temp_min)}&deg;Cel</h6> : <h6></h6>}
-                  <h6>min</h6>
+              <div className=' row weatherReport mb-3'>
+                <div className='col-sm-4 boxes shadow'>
+                  <div>
+                    {city.main ? <h6 className='text-center fw-normal '>{Math.trunc(city.main.temp_min)}&deg;Cel</h6> : <h6></h6>}
+                    <h6 className='text-center fw-normal'><FontAwesomeIcon icon={faTemperatureLow} /> min</h6>
+                  </div>
                 </div>
-                <div>
-                  {city.main ? <h6>{Math.trunc(city.main.temp_max)}&deg;Cel</h6> : <h6></h6>}
-                  <h6>max</h6>
+                <div className='col-sm-4 '></div>
+                <div className='col-sm-4 boxes shadow'>
+                  <div>
+                    {city.main ? <h6 className='text-center fw-normal'>{Math.trunc(city.main.temp_max)}&deg;Cel</h6> : <h6></h6>}
+                    <h6 className='text-center fw-normal'><FontAwesomeIcon icon={faTemperatureHigh} /> max</h6>
+                  </div>
                 </div>
               </div>
 
-              <div className='weatherReport'>
-                <div>
-                  {city.main ? <h6>{city.main.humidity}%</h6> : <h6></h6>}
-                  <h6>Humidity</h6>
+              <div className='row weatherReport'>
+                <div className='col-sm-4 boxes shadow'>
+                  <div>
+                    {city.main ? <h6 className='text-center fw-normal'>{city.main.humidity}%</h6> : <h6></h6>}
+                    <h6 className='text-center fw-normal'><FontAwesomeIcon icon={faDroplet} /> Humidity</h6>
+                  </div>
                 </div>
-                <div>
-                  {city.wind ? <h6>{Math.trunc((city.wind.speed)*18/5)}km/h</h6> : <h6></h6>}
-                  <h6>Windspeed</h6>
+                <div className='col-sm-4 boxes shadow'>
+                  <div>
+                    {city.wind ? <h6 className='text-center fw-normal'>{Math.trunc((city.wind.speed)*18/5)}km/h</h6> : <h6></h6>}
+                    <h6 className='text-center fw-normal'><FontAwesomeIcon icon={faWind} /> Windspeed</h6>
+                  </div>
                 </div>
-                <div>
-                  {city.main ? <h6>{Math.trunc(city.main.feels_like)}&deg;Cel</h6> : <h6></h6>}
-                  <h6>Feels like</h6>
+                <div className='col-sm-4 boxes shadow'>
+                  <div>
+                    {city.main ? <h6 className='text-center fw-normal'>{Math.trunc(city.main.feels_like)}&deg;Cel</h6> : <h6></h6>}
+                    <h6 className='text-center fw-normal'><FontAwesomeIcon icon={faThermometer} /> Feels like</h6>
+                  </div>
                 </div>
               </div>
           </div>
           <hr />
         </div>
-           {/* <div className='col-sm-4'>
-           <div className='weather'>
-            <FontAwesomeIcon icon={faCloudBolt} className='weatherIcon'/>
-             <div>
-              {city.main ? <h1>{Math.trunc(city.main.temp)}&deg;Cel</h1> : <h1></h1>}
-             </div>
-           </div>
-           <div className='weatherType'>
-              {city.weather && city.weather.length > 0 ? (
-                <h1>{city.weather[0].main}</h1>
-              ) : (
-                <h1></h1>
-              )}
-           </div>
-           </div>
-          
-          </div>
-          <div className='col-sm-4 ' >
-            <div className='row weatherReport mt-2'>
-              {city.main ? <h6>{Math.trunc(city.main.temp_min)}&deg;Cel</h6> : <h6></h6>}
-              <h6>min</h6>
-              {city.main ? <h6>{city.main.humidity}%</h6> : <h6></h6>}
-              <h6>Humidity</h6> 
-              {city.sys ? <h6>{city.sys.sunrise}</h6> : <h6></h6>}
-              <h6>Sunrise</h6>
-            </div>
-            <div className='row weatherReport mt-2'>
-              {city.main ? <h6>{Math.trunc(city.main.temp_max)}&deg;Cel</h6> : <h6></h6>}
-              <h6>max</h6>
-              {city.main ? <h6>{Math.trunc(city.main.feels_like)}&deg;Cel</h6> : <h6></h6>}
-              <h6>Feels like</h6>
-              {city.wind ? <h6>{Math.trunc((city.wind.speed)*18/5)}km/h</h6> : <h6></h6>}
-              <h6>Windspeed</h6>
-              {city.sys ? <h6>{city.sys.sunset}</h6> : <h6></h6>}
-              <h6>Sunset</h6>
-            </div>
-          </div>
-           <hr /> */}
-
-        
       </div>
     </div>
   )
